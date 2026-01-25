b20c3352f0c3966b749aaa5189a3f037765c83bf
Pietrols <kabambapeter3@gmail.com>
2026-01-26 00:56:34 +0200

    feat: add header, footer and layout components

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <h2>2026 Interview Prep Team</h2>;
+}
+
+export default Footer;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>Interview Prep App</h1>;
+}
+
+export default Header;
