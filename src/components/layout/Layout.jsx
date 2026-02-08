3ec83b89a80debe7011eb0b6bae9d2857f56b51e
Peter Kabamba <kabambapeter3@gmail.com>
2026-02-08 23:42:14 +0200

    style: fixed alignment

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,11 +4,11 @@ import Footer from "./Footer";
 
 function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col ">
       <header className="bg-white shadow-sm">
         <Header />
       </header>
-      <main className="flex-1 container px-4 py-4">
+      <main className="container mx-auto px-4 py-4">
         <Outlet />
       </main>
       <footer className="bg-gray-100 border-gray-200">
